9a395b52a0afb5470da31071a351436e1763e304
suppa <99502253+slxer1234483883@users.noreply.github.com>
2024-09-16 00:12:35 +0530

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xhae14@15fr-)j(sl&t$vrvo*v60^ks(0xth@bn*@5-e%wwl)f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["appreciate-assignment.onrender.com", 'fruit-ai-0bco.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["appreciate-assignment.onrender.com", 'server-00jm.onrender.com', 'localhost', '127.0.0.1']
 
 
 
